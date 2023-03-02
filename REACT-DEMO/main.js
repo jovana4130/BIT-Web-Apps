885b8ce9379904cd47d98bd80d15f2e383aa5068
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-02 19:40:14 +0100

    Class update

--- REACT-DEMO/main.js
+++ REACT-DEMO/main.js
@@ -4,4 +4,29 @@ const reactRootElement = ReactDOM.createRoot(rootElement);
 
 
 
-const paragraphReactElement = React.createElement('p', {className: 'title'}, null, 'Hello World');
+const helloSpanElement = React.createElement('span', null, 'Hello');
+const worldSpanElement = React.createElement('span', null, 'World');
+const paragraphReactElement = React.createElement(
+    'ul', 
+    null, 
+    React.createElement('li', null, React.createElement('p', null, Item 1')),
+    React.createElement('li', null, React.createElement('p', null, Item 1')),
+);
+
+
+
+
+const paragraphReactElement = (
+    <ul>
+        <li><p>Item 1</p></li>
+        <li><p>Item 2</p></li>
+        <li><p>Item 3</p></li>
+        <li><p>Item 4</p></li>
+        <li><p>Item 5</p></li>
+        <li><p>Item 6</p></li>
+        <li><p>Item 7</p></li>
+        <li><p>Item 8</p></li>
+    </ul>
+);
+
+reactRootElement.render(paragraphReactElement);
