1add3eff3bd9d87f22916dadd519aa4e718ebbb6
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-02 19:26:16 +0100

    Class demo

--- REACT-DEMO/main.js
+++ REACT-DEMO/main.js
@@ -1,2 +1,7 @@
 const rootElement = document.getElementById('root');
 
+const reactRootElement = ReactDOM.createRoot(rootElement);
+
+
+
+const paragraphReactElement = React.createElement('p', {className: 'title'}, null, 'Hello World');
