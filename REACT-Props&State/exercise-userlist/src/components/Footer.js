817b31cd2f251dcc3e3fccfb995a44c79eb71644
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-13 18:36:19 +0100

    Header and Footer

---
+++ REACT-Props&State/exercise-userlist/src/components/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="text-center p-4">
+                <small>
+                    © 2023 Copyright
+                    <a className="bit-link" href="https://www.bgit.rs/en/"> BIT</a>
+                </small>
+            </div>
+        </footer>
+    );
+};
---
+++ REACT-Props&State/exercise-userlist/src/components/Header.js
@@ -0,0 +1,6 @@
+import './Header.css';
+
+export const Header = () => 
+<nav className="navbar navbar-light bg-light justify-content-center">
+    <div id="header">React Users</div>
+</nav>;
